564af830c7ea0f22c997bbf64b1c68fcd7bf435e
Mario Uher <uher.mario@gmail.com>
2022-10-21 10:25:09 +0200

    Make worker more useful

--- lib/app/worker.ex
+++ lib/app/worker.ex
@@ -3,18 +3,27 @@ defmodule App.Worker do
   require Logger
 
   def start_link([name]) do
-    case GenServer.start_link(__MODULE__, [], name: via_tuple(name)) do
+    case GenServer.start_link(__MODULE__, name, name: via_tuple(name)) do
       {:ok, pid} ->
         {:ok, pid}
 
       {:error, {:already_started, pid}} ->
-        Logger.info("Already started at #{inspect(pid)}, returning :ignore")
+        Logger.info("Already started #{name} at #{inspect(pid)}, returning :ignore")
         :ignore
     end
   end
 
-  def init(_args) do
-    {:ok, nil}
+  def init(name) do
+    Process.send_after(self(), :tick, 1000)
+    {:ok, {name, 0}}
+  end
+
+  def handle_info(:tick, {name, count}) do
+    count = count + 1
+    Logger.info("#{name} (#{inspect(self())}): #{count}")
+
+    Process.send_after(self(), :tick, 1000)
+    {:noreply, {name, count}}
   end
 
   def via_tuple(name), do: {:via, Horde.Registry, {App.WorkerRegistry, name}}
